b796e4836deca5bf07d8d052dc05ba6a5c8466da
Junyo <130440930+SSub-jun@users.noreply.github.com>
2024-11-14 17:57:57 +0900

    Initial Commit

---
+++ PepperoniV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PepperoniV2
+//
+//  Created by 변준섭 on 11/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PepperoniV2/PepperoniV2App.swift
@@ -0,0 +1,17 @@
+//
+//  PepperoniV2App.swift
+//  PepperoniV2
+//
+//  Created by 변준섭 on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct PepperoniV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
