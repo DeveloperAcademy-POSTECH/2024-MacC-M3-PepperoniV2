9606545df43994021355a624601fab3afa516f29
HyunJaeyeon <bomul0109@gmail.com>
2024-11-18 23:29:31 +0900

    feat: #4-저장 전 임시 데이터 설정
    - 저장 안누르고 x버튼 누르면 이전 데이터가 그대로 유지됨
    - tempGameData로 관리

--- PepperoniV2/Presentation/GameSetting/PlayerSettingViewModel.swift
+++ PepperoniV2/Presentation/GameSetting/PlayerSettingViewModel.swift
@@ -9,32 +9,43 @@ import SwiftUI
 
 @Observable class PlayerSettingViewModel {
     private(set) var gameData: GameData
+    private(set) var tempGameData: GameData
 
     init(gameData: GameData) {
         self.gameData = gameData
+        
+        // 저장 전 임시 GameData
+        self.tempGameData = GameData()
+        self.tempGameData.players = gameData.players
     }
 
     /// 플레이어 추가
     func addPlayer() {
-        let newPlayer = Player(nickname: "\(gameData.players.count + 1)번", turn: gameData.players.count + 1)
-        gameData.players.append(newPlayer)
+        let newPlayer = Player(nickname: "\(tempGameData.players.count + 1)번", turn: tempGameData.players.count + 1)
+        tempGameData.players.append(newPlayer)
     }
 
     /// 플레이어 제거
     func removePlayer() {
-        if !gameData.players.isEmpty {
-            gameData.players.removeLast()
+        if !tempGameData.players.isEmpty {
+            tempGameData.players.removeLast()
         }
     }
 
     /// 닉네임 업데이트
     func updateNickname(for index: Int, nickname: String) {
-        guard index < gameData.players.count else { return }
-        gameData.players[index].nickname = nickname
+        guard index < tempGameData.players.count else { return }
+        tempGameData.players[index].nickname = nickname
     }
     
     /// 플레이어 초기화
     func resetPlayer() {
-        gameData.players = [Player(nickname: "1번", turn: 1)] // 초기 상태
+        tempGameData.players = [Player(nickname: "1번", turn: 1)]
+    }
+    
+    /// 변경사항 저장
+    func saveChanges() {
+        gameData.players = tempGameData.players
+        gameData.selectedAnime = tempGameData.selectedAnime
     }
 }
