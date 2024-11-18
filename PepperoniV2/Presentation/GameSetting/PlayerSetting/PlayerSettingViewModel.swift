20e3b2748da929be2b5f13366d1fb90872365ab1
HyunJaeyeon <bomul0109@gmail.com>
2024-11-19 02:12:08 +0900

    refactor: #7-PlayerSetting 코드 리팩토링
    - tempGameData -> tempPlayers

--- PepperoniV2/Presentation/GameSetting/PlayerSetting/PlayerSettingViewModel.swift
+++ PepperoniV2/Presentation/GameSetting/PlayerSetting/PlayerSettingViewModel.swift
@@ -9,43 +9,41 @@ import SwiftUI
 
 @Observable class PlayerSettingViewModel {
     private(set) var gameData: GameData
-    private(set) var tempGameData: GameData
+    private(set) var tempPlayers: [Player]
 
     init(gameData: GameData) {
         self.gameData = gameData
         
         // 저장 전 임시 GameData
-        self.tempGameData = GameData()
-        self.tempGameData.players = gameData.players
+        self.tempPlayers = gameData.players
     }
 
     /// 플레이어 추가
     func addPlayer() {
-        let newPlayer = Player(nickname: "\(tempGameData.players.count + 1)번", turn: tempGameData.players.count + 1)
-        tempGameData.players.append(newPlayer)
+        let newPlayer = Player(nickname: "\(tempPlayers.count + 1)번", turn: tempPlayers.count + 1)
+        tempPlayers.append(newPlayer)
     }
 
     /// 플레이어 제거
     func removePlayer() {
-        if !tempGameData.players.isEmpty {
-            tempGameData.players.removeLast()
+        if !tempPlayers.isEmpty {
+            tempPlayers.removeLast()
         }
     }
 
     /// 닉네임 업데이트
     func updateNickname(for index: Int, nickname: String) {
-        guard index < tempGameData.players.count else { return }
-        tempGameData.players[index].nickname = nickname
+        guard index < tempPlayers.count else { return }
+        tempPlayers[index].nickname = nickname
     }
     
     /// 플레이어 초기화
     func resetPlayer() {
-        tempGameData.players = [Player(nickname: "1번", turn: 1)]
+        tempPlayers = [Player(nickname: "1번", turn: 1)]
     }
     
     /// 변경사항 저장
     func saveChanges() {
-        gameData.players = tempGameData.players
-        gameData.selectedAnime = tempGameData.selectedAnime
+        gameData.players = tempPlayers
     }
 }
