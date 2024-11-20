8cc4fcc1b4c976d76b9c5df3240128440f74dc2c
Junyo <130440930+SSub-jun@users.noreply.github.com>
2024-11-20 17:51:09 +0900

    feat: #17 권한 설정 turnSettingVIew에서 alert why turnsettingView? - 게임 플로우가 방해 받지 않았으면 좋겠음! - 룰렛 돌리기 - 유튜브 - 말하기! 의 플로우 전에 되었으면 좋겠다는 이유

--- PepperoniV2/Presentation/Game/TurnSettingView.swift
+++ PepperoniV2/Presentation/Game/TurnSettingView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import AVFoundation
+import Speech
 
 struct TurnSettingView: View {
     @EnvironmentObject var router: Router
@@ -43,6 +45,37 @@ struct TurnSettingView: View {
             }
             .onTapGesture {
                 manager.spinRoulette()
+            }
+            .onAppear{
+                // 마이크 권한 요청
+                AVAudioSession.sharedInstance().requestRecordPermission { granted in
+                    if granted {
+                        print("마이크 접근 권한이 허용되었습니다.")
+                        
+                        // 음성 인식 권한 요청
+                        // TODO: Speech 권한 허용 위해 Speech를 추가, 구조 분리-변경 필요
+                        // TODO: 사용자가 권한 거부할 경우 권한 켜도록 유도 필요(온보딩 만들어주기 or 거부했을 경우 설정에서 켜게 하기)
+                        SFSpeechRecognizer.requestAuthorization { authStatus in
+                            DispatchQueue.main.async {
+                                switch authStatus {
+                                case .authorized:
+                                    print("음성 인식 권한이 허용되었습니다.")
+                                case .denied:
+                                    print("음성 인식 권한이 거부되었습니다.")
+                                case .restricted:
+                                    print("음성 인식이 제한되었습니다.")
+                                case .notDetermined:
+                                    print("음성 인식 권한이 설정되지 않았습니다.")
+                                @unknown default:
+                                    break
+                                }
+                            }
+                        }
+                        
+                    } else {
+                        print("마이크 접근 권한이 거부되었습니다.")
+                    }
+                }
             }
         }
         Button {
