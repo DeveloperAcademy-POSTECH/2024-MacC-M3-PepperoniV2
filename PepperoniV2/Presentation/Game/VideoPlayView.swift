f09cb90f38868eee10a9c737512f2d860249383d
Junyo <130440930+SSub-jun@users.noreply.github.com>
2024-11-20 20:30:42 +0900

    feat: #19 - VideoPlayView UI 구현 완 추후 컬러와 폰트 적용 예정

--- PepperoniV2/Presentation/Game/VideoPlayView.swift
+++ PepperoniV2/Presentation/Game/VideoPlayView.swift
@@ -11,41 +11,73 @@ struct VideoPlayView: View {
     @EnvironmentObject var router: Router
     @Environment(GameViewModel.self) var gameViewModel
     
-    // TODO: 유튜브 속성 임의로 설정 - 추후 삭제
-    @State private var youtubeID = "Ue_JMHDEds4"
-    @State private var youtubeStartTime = 40
-    @State private var youtubeEndTime = 50
-    
     @State private var replayTrigger = false
     
-    
     var body: some View {
-        if let selectedQuote = gameViewModel.selectedQuote{
-            YouTubePlayerView(
-                videoID: selectedQuote.youtubeID,
-                startTime: Int(selectedQuote.youtubeStartTime),
-                endTime: Int(selectedQuote.youtubeEndTime),
-                replayTrigger: replayTrigger
-            )
-            .frame(height: 218)
-            .padding(.bottom, 24)
-        }
-        
-        
-        Button(action: {
-            replayTrigger.toggle()
-        }) {
-            Text("Replay")
-        }
-        
-        Button {
-            router.push(screen: Game.speaking)
-        } label: {
-            Text("다음")
+        VStack{
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.gray, lineWidth: 2)
+                .frame(height: 78)
+                .overlay{
+                    VStack(spacing:6){
+                        HStack{
+                            Image(systemName: "exclamationmark.triangle.fill")
+                            Text("주의")
+                        }
+                        Text("애니 내용 스포가 포함되어 있습니다.")
+                            .foregroundStyle(.gray)
+                    }
+                }
+                .padding(.init(top: 0, leading: 36, bottom: 28, trailing: 36))
+            
+            if let selectedQuote = gameViewModel.selectedQuote{
+                YouTubePlayerView(
+                    videoID: selectedQuote.youtubeID,
+                    startTime: Int(selectedQuote.youtubeStartTime),
+                    endTime: Int(selectedQuote.youtubeEndTime),
+                    replayTrigger: replayTrigger
+                )
+                .frame(height: 218)
+                .padding(.bottom, 24)
+            }
+            
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.gray, lineWidth: 2)
+                .frame(height: 50)
+                .overlay{
+                    VStack(spacing:6){
+                        Text("참고용 영상 안의 대사를 따라 말할 예정입니다.")
+                            .foregroundStyle(.gray)
+                    }
+                }
+                .padding(.init(top: 0, leading: 16, bottom: 131, trailing: 16))
+            
+            Button(action: {
+                replayTrigger.toggle()
+            }) {
+                Image("YoutubeReplayButton")
+                    .resizable()
+                    .frame(width:109, height:41)
+                    .padding(.bottom, 28)
+            }
+            
+            Button {
+                router.push(screen: Game.speaking)
+            } label: {
+                RoundedRectangle(cornerRadius: 60)
+                    .frame(height:54)
+                    .overlay{
+                        Text("젠데이아 첫번째 시작")
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.horizontal, 16)
+                
+            }
         }
     }
 }
 
-#Preview {
-    VideoPlayView()
-}
+//#Preview {
+//    VideoPlayView()
+//        .preferredColorScheme(.dark)
+//}
