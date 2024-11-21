80f1cc22b7b7eb69697f96ac111e8684aa550466
Junyo <130440930+SSub-jun@users.noreply.github.com>
2024-11-21 18:52:48 +0900

    feat: #23-SpeakingViewUI 컬러 및 폰트 적용

--- PepperoniV2/Presentation/Game/SpeakingView.swift
+++ PepperoniV2/Presentation/Game/SpeakingView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 struct SpeakingView: View {
-    @EnvironmentObject var router: Router
-    @Environment(GameViewModel.self) var gameViewModel
-
+        @EnvironmentObject var router: Router
+        @Environment(GameViewModel.self) var gameViewModel
+    
     // TODO: - Preview용 gameviewmodel, 컬러 폰트 적용 후 삭제
 //    @State var gameViewModel: GameViewModel = GameViewModel(
 //        selectedAnime: Anime(
@@ -45,10 +45,10 @@ struct SpeakingView: View {
 //        ),
 //        selectedQuote: AnimeQuote(
 //            id: "quote2",
-//            japanese: ["お前が決！", "めるんだ"],
-//            pronunciation: ["오마에가", "키메룬다!"],
-//            korean: ["네가 결정하는", "거야!"],
-//            timeMark: [0.0, 1.5],
+//            japanese: ["お前が決！", "めるんだ", "んだ", "んだ", "んだ", "んだ"],
+//            pronunciation: ["오마에가", "키메룬다!","오마에가", "키메룬다!","오마에가", "키메룬다!"],
+//            korean: ["네가 결정하는", "거야!", "가나", "다라", "마바", "사아"],
+//            timeMark: [0.1, 1.5],
 //            voicingTime: 2.5,
 //            audioFile: "attack_on_titan_quote2.mp3",
 //            youtubeID: "abcd1234",
@@ -86,28 +86,54 @@ struct SpeakingView: View {
                 .padding(.bottom, 14)
                 
                 Text("\(gameViewModel.players.count)명중 \(gameViewModel.players[gameViewModel.turnComplete].turn+1)번째")
-                    .font(.system(size: 14))
+                    .suit(.medium, size: 14)
                     .padding(.bottom, 6)
                 
                 Text("\(gameViewModel.players[gameViewModel.turnComplete].nickname ?? "") 차례")
-                    .font(.system(size: 20))
-                    .padding(.bottom, 38)
-                
-                Rectangle()
-                    .frame(height:1)
-                    .foregroundStyle(.gray)
-                    .padding(.horizontal, 17.5)
+                    .hakgyoansim(size: 20)
                 
                 Spacer()
                 
-                if let quote = gameViewModel.selectedQuote{
-                    if quote.japanese.count >= 5 {
-                        let halfIndex = quote.japanese.count / 2
-                        
-                        // 두 개의 HStack으로 나누어 텍스트 표시
-                        VStack {
+                ZStack{
+                    LinearGradient(
+                        gradient: Gradient(colors: [Color.black.opacity(0), Color.ppDarkGray_03]),
+                        startPoint: .top, // 대각선 시작점
+                        endPoint: .bottom // 대각선 끝점
+                    )
+                    
+                    if let quote = gameViewModel.selectedQuote{
+                        if quote.japanese.count >= 5 {
+                            let halfIndex = quote.japanese.count / 2
+                            
+                            // 두 개의 HStack으로 나누어 텍스트 표시
+                            VStack(spacing:32) {
+                                HStack {
+                                    ForEach(0..<halfIndex, id: \.self) { index in
+                                        WordCard(
+                                            pronunciation: quote.pronunciation[index],
+                                            japanese: quote.japanese[index],
+                                            isHighlighted: isHighlighted(wordIndex: index, timeByWord: quote.timeMark, timerCount: timerCount)
+                                        )
+                                    }
+                                }
+                                .padding()
+                                
+                                HStack {
+                                    ForEach(halfIndex..<quote.japanese.count, id: \.self) { index in
+                                        WordCard(
+                                            pronunciation: quote.pronunciation[index],
+                                            japanese: quote.japanese[index],
+                                            isHighlighted: isHighlighted(wordIndex: index, timeByWord: quote.timeMark, timerCount: timerCount)
+                                        )
+                                    }
+                                }
+                                .padding(.bottom, 50)
+                            }
+                            
+                        } else {
+                            // 길이가 5 미만일 때 기존 방식
                             HStack {
-                                ForEach(0..<halfIndex, id: \.self) { index in
+                                ForEach(quote.japanese.indices, id: \.self) { index in
                                     WordCard(
                                         pronunciation: quote.pronunciation[index],
                                         japanese: quote.japanese[index],
@@ -116,80 +142,102 @@ struct SpeakingView: View {
                                 }
                             }
                             .padding()
-                            
-                            HStack {
-                                ForEach(halfIndex..<quote.japanese.count, id: \.self) { index in
-                                    WordCard(
-                                        pronunciation: quote.pronunciation[index],
-                                        japanese: quote.japanese[index],
-                                        isHighlighted: isHighlighted(wordIndex: index, timeByWord: quote.timeMark, timerCount: timerCount)
-                                    )
-                                }
-                            }
-                            .padding(.bottom, 50)
                         }
-                        
-                    } else {
-                        // 길이가 5 미만일 때 기존 방식
-                        HStack {
-                            ForEach(quote.japanese.indices, id: \.self) { index in
-                                WordCard(
-                                    pronunciation: quote.pronunciation[index],
-                                    japanese: quote.japanese[index],
-                                    isHighlighted: isHighlighted(wordIndex: index, timeByWord: quote.timeMark, timerCount: timerCount)
-                                )
-                            }
-                        }
-                        .padding()
                     }
                 }
+                .padding(.init(top: 50, leading: 0, bottom: 58, trailing: 0))
+                
                 
                 Spacer()
-                
-                Rectangle()
-                    .frame(height:1)
-                    .foregroundStyle(.gray)
-                    .padding(.init(top: 0, leading: 17.5, bottom: 65, trailing: 17.5))
                 
                 VStack{
                     Button(action:{
                         Task {
-//                            await sttManager.stopRecording()  // stopRecoding() 동기 처리
+                            await sttManager.stopRecording()  // stopRecoding() 동기 처리
                             stopTimer()
                             grading()
-//                            router.push(screen: Game.score)
+                            router.push(screen: Game.score)
                         }
                     }, label:{
                         Image("SpeakingStopButton")
                             .resizable()
                             .frame(width:175, height:64)
                     })
-                    .padding(.bottom, 78)
+                    .padding(.bottom, 40)
                 }
                 
             }
             if !isCounting {
                 RoundedRectangle(cornerRadius: 60)
                     .stroke(
-                            LinearGradient(
-                                colors: [.blue, .green, .purple], // 그라데이션 색상
-                                startPoint: .topLeading,         // 시작점
-                                endPoint: .bottomTrailing        // 끝점
-                            ),
-                            lineWidth: 6 // 선의 굵기
-                        )
-                    
+                        LinearGradient(
+                            gradient: Gradient(stops: [
+                                Gradient.Stop(color: Color(hex: "3FE9FF"), location: 0.00),
+                                Gradient.Stop(color: Color(hex: "6652E7"), location: 0.75),
+                                Gradient.Stop(color: Color(hex: "AD29FF"), location: 1.0)
+                            ]),
+                            startPoint: .topLeading, // 시작점
+                            endPoint: .bottomTrailing // 끝점
+                        ),
+                        
+                        lineWidth: 6 // 선의 굵기
+                    )
+                
                     .padding(2)
                     .ignoresSafeArea()
-                    
+                
             }
             if isCounting {
-                Color.black.opacity(0.7) // 어두운 오버레이 배경
+                Color.black // 어두운 오버레이 배경
                     .edgesIgnoringSafeArea(.all)
                 
-                Text(countdown > 0 ? "\(countdown)" : "Start!")
-                    .font(.system(size: 100, weight: .bold))
-                    .foregroundColor(.white)
+                VStack{
+                    Text("\(gameViewModel.players.count)명중 \(gameViewModel.players[gameViewModel.turnComplete].turn+1)번째")
+                        .suit(.medium, size: 14)
+                        .padding(.bottom, 6)
+                    
+                    Text("\(gameViewModel.players[gameViewModel.turnComplete].nickname ?? "") 차례")
+                        .hakgyoansim(size: 20)
+                        .padding(.bottom, 57)
+                    
+                    Circle()
+                        .stroke(lineWidth: 14)
+                        .frame(width:290, height:290)
+                        .foregroundStyle(
+                            
+                            EllipticalGradient(
+                                stops: [
+                                    Gradient.Stop(color: Color(hex:"AD29FF"), location: 0.00),
+                                    Gradient.Stop(color: Color(hex:"6652E7"), location: 0.36),
+                                    Gradient.Stop(color: Color(hex:"3FE9FF"), location: 1.00),
+                                ],
+                                center: UnitPoint(x: 0.53, y: -0.04),
+                                startRadiusFraction: 0.1,
+                                endRadiusFraction: 1
+                            )
+                        )
+                        .overlay{
+                            if countdown > 0 {
+                                Text("\(countdown)")
+                                    .hakgyoansim(size: 200)
+                                    .foregroundStyle(.white)
+                            } else {
+                                Text("START")
+                                    .hakgyoansim(size: 70)
+                                    .foregroundStyle(
+                                        LinearGradient(
+                                            gradient: Gradient(stops: [
+                                                Gradient.Stop(color: Color(hex: "AD29FF"), location: 0.06), // 시작점
+                                                Gradient.Stop(color: Color(hex: "FFFFFF"), location: 0.44),
+                                                Gradient.Stop(color: Color(hex: "3FE9FF"), location: 1.0)// 끝점
+                                            ]),
+                                            startPoint: .topTrailing,  // 대각선 시작점
+                                            endPoint: .bottomLeading   // 대각선 끝점
+                                        )
+                                    )
+                            }
+                        }
+                }
             }
         }
         
@@ -209,7 +257,7 @@ struct SpeakingView: View {
             Alert(
                 title: Text("홈 화면으로 나가시겠습니까?"),
                 primaryButton: .destructive(Text("나가기")) {
-                    router.popToRoot()
+                    //                    router.popToRoot()
                 },
                 secondaryButton: .cancel(Text("취소"))
             )
@@ -224,7 +272,7 @@ struct SpeakingView: View {
             } else if countdown == 1 {
                 countdown -= 1
                 
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                     self.isCounting = false
                     timer.invalidate()
                     sttManager.startRecording() // STT 녹음 시작
@@ -290,18 +338,25 @@ struct WordCard: View {
     var body: some View {
         VStack(spacing: 12) {
             Text(pronunciation)
-                .font(.system(size: 18))
+                .suit(.bold, size: 18)
+                .foregroundStyle(isHighlighted ? Color.black : Color.ppMint_00)
             Text(japanese)
-                .font(.system(size: 14))
+                .foregroundStyle(isHighlighted ? Color.ppPurple_02 : Color.ppWhiteGray)
         }
         .bold()
         .padding(8)
         .background(
-            isHighlighted
-            ? RoundedRectangle(cornerRadius: 4)
-                .fill(Color.blue.opacity(0.5))
-                .frame(height: 88)
-            : nil
+            Group {
+                if isHighlighted {
+                    RoundedRectangle(cornerRadius: 4)
+                        .fill(Color.ppMint_00)
+                } else {
+                    RoundedRectangle(cornerRadius: 4)
+                        .stroke(style: StrokeStyle(lineWidth: 2, dash: [5, 5]))
+                        .foregroundStyle(Color.ppDarkGray_02)
+                }
+            }
+                .frame(height: 88) // frame을 Group에 적용
         )
     }
 }
